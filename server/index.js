a5605fa7c06c9e60e2a3c0a5654666fa1013c504
Johnny Chen <mcjohnnychen@gmail.com>
2017-11-10 19:04:20 -0800

    Add react and webpack loader

---
+++ server/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+var port = 3000;
+app.listen(port);
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/', function(req, res) {
+  // res.send('hello, world! boo!zee');
+  // res.render('index');
+})
